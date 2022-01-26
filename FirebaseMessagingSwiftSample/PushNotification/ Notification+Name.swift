63828f76a3e606e5dad3fbab9ca88f9e40360e38
Amr Elghadban <amr.elghadban@gmail.com>
2022-01-26 12:58:29 +0400

    author credit

--- FirebaseMessagingSwiftSample/PushNotification/ Notification+Name.swift
+++ FirebaseMessagingSwiftSample/PushNotification/ Notification+Name.swift
@@ -1,8 +1,14 @@
 //
-//   Notification+Name.swift
-//  FirebaseMessagingSwiftSample
+//  Notification+Name.swift
+//  BookStore
 //
-//  Created by Amr Elghadban on 26/01/2022.
+//  Created by Amr Elghadban on 27/12/2021.
+//  Copyright © 2021 ADKA Tech. All rights reserved.
 //
 
 import Foundation
+
+extension Notification.Name {
+    /// Notification.Name for pushNotificationReceived
+    static let pushNotificationReceived = Notification.Name("PushNotificationReceived")    
+}
